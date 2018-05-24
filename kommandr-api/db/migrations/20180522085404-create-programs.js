6488f3962b11477848640b312f05372c3ef3a61d
Eddie Ramirez <djedi.r@gmail.com>
2018-05-24 00:00:18 -0700

    Defined charsets in tables

--- kommandr-api/db/migrations/20180522085404-create-programs.js
+++ kommandr-api/db/migrations/20180522085404-create-programs.js
@@ -1,65 +1,71 @@
 module.exports = {
   up: function(sequelize, DataTypes) {
-    return sequelize.createTable("Programs", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
-      },
-      sourceId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-          model: "Sources",
-          key: "id"
+    return sequelize.createTable(
+      "Programs",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER
+        },
+        sourceId: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+          references: {
+            model: "Sources",
+            key: "id"
+          }
+        },
+        platformId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: "Platforms",
+            key: "id"
+          }
+        },
+        cliName: {
+          type: DataTypes.STRING(64),
+          allowNull: false
+        },
+        name: {
+          type: DataTypes.STRING(64),
+          allowNull: false
+        },
+        rawManPage: {
+          type: DataTypes.STRING
+        },
+        linkUrl: {
+          type: DataTypes.STRING(250)
+        },
+        shortDescription: {
+          type: DataTypes.STRING(300)
+        },
+        totalViews: {
+          type: DataTypes.INTEGER,
+          defaultValue: 0
+        },
+        totalCommands: {
+          type: DataTypes.INTEGER,
+          defaultValue: 0
+        },
+        status: {
+          type: DataTypes.INTEGER
+        },
+        createdAt: {
+          type: DataTypes.DATE
+        },
+        updatedAt: {
+          type: DataTypes.DATE
+        },
+        deletedAt: {
+          type: DataTypes.DATE
         }
       },
-      platformId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "Platforms",
-          key: "id"
-        }
-      },
-      cliName: {
-        type: DataTypes.STRING(64),
-        allowNull: false
-      },
-      name: {
-        type: DataTypes.STRING(64),
-        allowNull: false
-      },
-      rawManPage: {
-        type: DataTypes.STRING
-      },
-      linkUrl: {
-        type: DataTypes.STRING(250)
-      },
-      shortDescription: {
-        type: DataTypes.STRING(300)
-      },
-      totalViews: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-      },
-      totalCommands: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-      },
-      status: {
-        type: DataTypes.INTEGER
-      },
-      createdAt: {
-        type: DataTypes.DATE
-      },
-      updatedAt: {
-        type: DataTypes.DATE
-      },
-      deletedAt: {
-        type: DataTypes.DATE
+      {
+        charset: "utf8"
       }
-    });
+    );
   },
   down: function(sequelize, DataTypes) {
     return sequelize.dropTable("Programs");

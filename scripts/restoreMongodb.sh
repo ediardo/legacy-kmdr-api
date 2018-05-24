d11b29f3c9076e1e1e514cb92d638a102a3b5bf7
Eddie Ramirez <djedi.r@gmail.com>
2018-05-23 23:59:37 -0700

    Script for creating and populating databases

--- scripts/restoreMongodb.sh
+++ scripts/restoreMongodb.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-if [ ! -f $(pwd)/docker/mongodb/dumps/explainshell.tar.gz ]; then
-  curl -o $(pwd)/docker/mongodb/dumps/explainshell.tar.gz https://s3.amazonaws.com/kommandr.com/explainshell.tar.gz
+echo "Working on MariaDB instance"
+docker exec kommandr-api_graphql ./node_modules/.bin/sequelize db:create
+docker exec kommandr-api_graphql ./node_modules/.bin/sequelize db:migrate
+docker exec kommandr-api_graphql ./node_modules/.bin/sequelize db:seed:all
+echo ""
+
+echo "Working on MongoDB instance"
+if [ ! -f $(pwd)/dumps/explainshell.tar.gz ]; then
+  curl -o $(pwd)/dumps/explainshell.tar.gz https://s3.amazonaws.com/kommandr.com/explainshell.tar.gz
 fi
-docker create --name mongorestore mongo
-docker cp $(pwd)/docker/mongodb/dumps/explainshell.tar.gz mongorestore:/tmp
+docker run -d --name mongorestore --network kommandr-api_default mongo
+echo "Copying database backup into container..."
+docker cp $(pwd)/dumps/explainshell.tar.gz mongorestore:/tmp
+echo "Decompressing database backup file..."
+docker exec -it mongorestore bash -c "cd /tmp && tar xvzf explainshell.tar.gz"
+echo "Restoring backup into kommandr-api_mongodb container..."
+docker exec -it mongorestore bash -c "mongorestore -d explainshell --host kommandr-api_mongodb /tmp/explainshell"
+docker stop mongorestore
